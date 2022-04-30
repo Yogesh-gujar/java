1f1b847619ceec60707d22be784a051382220a66
yogi <badgujaryogesh12341@gmail.com>
2022-05-01 01:30:40 +0530

    add java first code hello world

---
+++ HelloWorld/helloWorld.java
@@ -0,0 +1,17 @@
+
+/* **************************************************************************************************************************************************** */
+//define the class fun here.
+class helloWorld {
+//Void fun define wich return null or value which provide by the user
+    public static void main(String[] args) {
+        System.out.println("hey, World!");  // if you comment this code and run still the void function give you the  null output.
+    }
+}   
+
+/* **************************************************************************************************************************************************** */
+
+/*
+
+
+
+*/
